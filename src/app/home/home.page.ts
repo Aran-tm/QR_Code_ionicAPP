e213b83a66e79e190cbd09f455786ee8bd8831b8
Aran-tm <armandortmontes@gmail.com>
2024-05-31 11:20:48 -0500

    lectura de imagen en moviles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 import html2canvas from 'html2canvas';
 
+// esto es para guardar imagenes en moviles, compartirlas
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -34,6 +38,31 @@ export class HomePage {
     link.href = canvas.toDataURL();
     link.download = 'qr.png';
     link.click();
+  }
+
+  // share image for mobile
+  async shareImage(canvas: HTMLCanvasElement) {
+
+    let base64 = canvas.toDataURL();
+    let path = 'qr.png';
+
+    // guarda la imagen en el dispositivo
+    await Filesystem.writeFile({
+      path: path,       // path va a ser el nombre con el que se va a guardar
+      data: base64,      // imagen que se va a leer
+      directory: Directory.Cache,     // directorio donde se va a almacenar
+    }).then(async (res) => {
+
+      let uri = res.uri;
+
+      await Share.share({
+        title: 'See cool stuff',
+        text: 'Really awesome thing you need to see right meow',
+        url: 'http://ionicframework.com/',
+        dialogTitle: 'Share with buddies',
+      });
+    })
+
   }
 
 }
