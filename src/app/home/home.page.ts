05a863cfa78bc60e8ba1ebd86d77800cb1c9c6f7
Aran-tm <armandortmontes@gmail.com>
2024-05-31 11:48:00 -0500

    construyendo proyecto android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import html2canvas from 'html2canvas';
 
 // esto es para guardar imagenes en moviles, compartirlas
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 import { Share } from '@capacitor/share';
+import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,9 @@ import { Share } from '@capacitor/share';
 })
 
 export class HomePage {
+
+  loadingController = inject(LoadingController);
+  platform = inject(Platform);    // para detectar las plataformas web, ios o android
 
   // esto es para establecer donde iniciar el segment
   segment = 'generate';
@@ -28,6 +32,12 @@ export class HomePage {
     html2canvas(element).then((canvas: HTMLCanvasElement) => {
 
       this.downloadImage(canvas);
+
+      // esto es para saber si es dispositivos moviles
+      if (this.platform.is('capacitor')) this.shareImage(canvas);
+
+      // esto es para la web
+      else this.downloadImage(canvas);
     });
   }
 
@@ -40,11 +50,20 @@ export class HomePage {
     link.click();
   }
 
+  // proceso asincrono Pueden tardar mientras se ejecutan
   // share image for mobile
   async shareImage(canvas: HTMLCanvasElement) {
 
     let base64 = canvas.toDataURL();
     let path = 'qr.png';
+
+    // loading de IONIC que demora 2 seg
+    const loading = await this.loadingController.create({
+      message: 'Hellooo',
+      duration: 2000,
+      spinner: 'crescent'
+    });
+    await loading.present();
 
     // guarda la imagen en el dispositivo
     await Filesystem.writeFile({
@@ -55,12 +74,19 @@ export class HomePage {
 
       let uri = res.uri;
 
+      // esto es para compartir la imagen en la uri
       await Share.share({
-        title: 'See cool stuff',
-        text: 'Really awesome thing you need to see right meow',
-        url: 'http://ionicframework.com/',
-        dialogTitle: 'Share with buddies',
+        url: uri,
       });
+
+      // borrar la imagen del cache para no desgastar el espacio
+      await Filesystem.deleteFile({
+        path: path,
+        directory: Directory.Cache,
+      })
+    }).finally(() => {    // este bloque se ejecuta cuando se finaliza el resto
+
+      loading.dismiss();
     })
 
   }
